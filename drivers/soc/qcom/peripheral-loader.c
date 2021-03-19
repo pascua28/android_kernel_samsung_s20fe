62255cbe3373fd90ed728db967edc49181a84067
Kristof Petho <kristof.petho@gmail.com>
2021-03-19 08:57:46 +0100

    peripheral-loader: fix missing ()
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- drivers/soc/qcom/peripheral-loader.c
+++ drivers/soc/qcom/peripheral-loader.c
@@ -291,7 +291,7 @@ static unsigned int prepare_minidump_segments(struct ramdump_segment *rd_segs,
 			rd_segs++;
 			val_segs++;
 		} else {
-			*ss_valid_seg_cnt--;
+			(*ss_valid_seg_cnt)--;
 		}
 
 		region_info++;
