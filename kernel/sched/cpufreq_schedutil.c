36e8f3a8567ddaa6c878a3e6d802fe7d8137cdda
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-07 01:54:53 -0400

    cpufreq/schedutil: Disable iowait boost when battery saver is active
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -17,6 +17,8 @@
 #include <trace/events/power.h>
 #include <linux/sched/sysctl.h>
 
+#include <linux/battery_saver.h>
+
 #ifdef CONFIG_SCHED_FFSI_GLUE
 #include <linux/ffsi.h>
 /**
@@ -608,7 +610,7 @@ static void sugov_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 		return;
 
 	/* Boost only tasks waking up after IO */
-	if (!set_iowait_boost)
+	if (!set_iowait_boost || is_battery_saver_on())
 		return;
 
 	/* Ensure boost doubles only one time at each request */
