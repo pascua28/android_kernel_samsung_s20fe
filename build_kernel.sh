90492936a04904c1e3f82bf4e1183c48c4223fad
Samuel Pascua <sgpascua@ngcp.ph>
2021-11-07 07:24:28 +0800

    build_env: update script, add option to compile for 5G variant
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,7 +8,41 @@ KERNEL_LLVM_BIN=/home/pascua14/clang/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+echo "**********************************"
+echo "Select variant (Snapdragon only)"
+echo "(1) 4G Variant"
+echo "(2) 5G Variant"
+read -p "Selected variant: " variant
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
+
+if [ $variant == "1" ]; then
+	echo "
+Compiling for 4G variant
+"
+
+elif [ $variant == "2" ]; then
+	echo "
+Compiling for 5G variant
+"
+	scripts/configcleaner "
+CONFIG_SAMSUNG_NFC
+CONFIG_NFC_PN547
+CONFIG_NFC_PN547_ESE_SUPPORT
+CONFIG_NFC_FEATURE_SN100U
+CONFIG_FIVE
+"
+
+	echo "
+# CONFIG_SAMSUNG_NFC is not set
+# CONFIG_NFC_PN547 is not set
+# CONFIG_NFC_PN547_ESE_SUPPORT is not set
+# CONFIG_NFC_FEATURE_SN100U is not set
+# CONFIG_FIVE is not set
+" >> out/.config
+
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
+
+fi
+
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE Image.gz-dtb
