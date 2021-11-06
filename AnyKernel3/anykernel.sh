544764101abd8de7b839faa29f20e73a17e4c788
Samuel Pascua <sgpascua@ngcp.ph>
2021-11-06 22:12:02 +0800

    build_env: add AnyKernel3
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,47 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Kernel for S20 FE by pascua28 @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=r8q
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/soc/1d84000.ufshc/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel boot install
+dump_boot;
+
+# begin ramdisk changes
+
+# end ramdisk changes
+
+write_boot;
+## end boot install
