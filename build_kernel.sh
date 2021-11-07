8ed19f5d556c1f2b1f06ab2d0a550b23481d1085
Samuel Pascua <sgpascua@ngcp.ph>
2021-11-07 17:08:32 +0800

    update build script
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+mkdir out
 
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
-export ARCH=arm64
 
-make ARCH=arm64 exynos9830-r8slte_defconfig
-make ARCH=arm64 -j64
+BUILD_CROSS_COMPILE=/home/pascua14/gcc-7.4.1/bin/aarch64-linux-gnu-
+KERNEL_LLVM_BIN=/home/pascua14/clang/bin/clang
+CLANG_TRIPLE=aarch64-linux-gnu-
+
+make O=out ARCH=arm64 CC=$KERNEL_LLVM_BIN exynos9830-r8slte_defconfig
+make O=out ARCH=arm64 \
+	CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN \
+	CLANG_TRIPLE=$CLANG_TRIPLE -j8 Image.gz-dtb
 
