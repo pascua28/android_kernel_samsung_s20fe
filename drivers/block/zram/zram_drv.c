a8eeaa2681173d62e7a1974e956a4068bf3513dc
Samuel Pascua <sgpascua@ngcp.ph>
2021-11-29 02:07:18 +0000

    block: zram: use lz4
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -48,7 +48,12 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
+
+#ifdef CONFIG_CRYPTO_LZ4
+static const char *default_compressor = "lz4";
+#else
 static const char *default_compressor = "lzo-rle";
+#endif
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
