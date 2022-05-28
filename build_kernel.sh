c532d49312d35335c413bf373a833e0d29fcf657
Elchanz <97859152+Elchanz3@users.noreply.github.com>
2022-05-28 04:25:16 -0300

    Add files via upload

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,8 +3,8 @@
 export ARCH=arm64
 mkdir out
 
-BUILD_CROSS_COMPILE=/home/pascua14/gcc-7.4.1/bin/aarch64-linux-gnu-
-KERNEL_LLVM_BIN=/home/pascua14/clang/bin/clang
+BUILD_CROSS_COMPILE=aarch64-linux-gnu-
+KERNEL_LLVM_BIN=/home/chanz/clang-14.0.7/clang-r445002/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
